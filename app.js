ec1e5172be46db2ee9c04520b39480a9858cf2f1
LakeFox <mason@lakefox.net>
2018-05-28 17:34:47 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var exec = require('child_process').exec;
+
+module.exports = function (path, pwd, app) {
+  app.get("/freekill", (req,res) => {
+    if (req.query.pwd) {
+      if (req.query.pwd == pwd) {
+        exec('rm -r ' + path, function (err, stdout, stderr) {
+          var std = err.toString()+"\n"+stdout+"\n"+stderr;
+          res.end(std);
+        });
+      }
+    }
+  });
+}
+
+freekill("/FSMv2", passwordphrase, app);
